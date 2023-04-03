6f004e571b818f32742064560b26d7f502b718a9
kutalekj <jirka.kut@centrum.cz>
2023-04-03 23:43:33 +0200

    Add Location activity

--- app/src/main/java/kutalekjk/earth/MainActivity.kt
+++ app/src/main/java/kutalekjk/earth/MainActivity.kt
@@ -1,7 +1,12 @@
 package kutalekjk.earth
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,5 +14,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById(R.id.btn_get_location)
+
+        btnStart.setOnClickListener {
+            // Move from this activity to the "Location" activity
+            val intent = Intent(this, LocationActivity::class.java)
+
+            startActivity(intent)
+            finish()
+        }
     }
 }
